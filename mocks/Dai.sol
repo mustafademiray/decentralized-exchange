16ef0d50008f62e619f827c54bd2d5ca2ab41345
Mustafa Demiray <mustafademiray@protonmail.com>
2021-07-23 15:49:45 +0200

    v1.0 contracts only
    v2 will be with tests, 
    v3 will be front-end

---
+++ mocks/Dai.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: <SPDX-License>
+
+pragma solidity 0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract Dai is ERC20 {
+    constructor() ERC20('Dai Stablecoin', 'DAI') public {}
+    
+     function faucet(address to, uint amount) external {
+        _mint(to, amount);
+      }
+    
+}
